1ef1426b6457cf5af606d8d6af2ffa957f59b81b
Hugo Oyola <hugo.ham@outlook.com>
2022-11-10 20:16:48 -0500

    Servicio para buscar países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,11 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class PorPaisComponent {
   termino:string = ''
 
-  constructor(){
+  constructor(private paisService: PaisService){
 
   }
 
   buscar(){
-    console.log('Buscar');
+    console.log(this.termino);
+    this.paisService.buscarPais(this.termino)
+      .subscribe(resp=>{
+        console.log(resp);
+      })
   }
 }
