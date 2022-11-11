2d22b5f62e6d1428ca91e3d648bdcbaafc5df872
Hugo Oyola <hugo.ham@outlook.com>
2022-11-10 22:13:35 -0500

    Llenar la tabla de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,6 +10,7 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) {}
 
@@ -16,11 +18,13 @@ export class PorPaisComponent {
     this.hayError = false;
     console.log(this.termino);
     this.paisService.buscarPais(this.termino).subscribe(
-      (resp) => {
-        console.log(resp);
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
       },
       (err) => {
         this.hayError = true;
+        this.paises = []
       }
     );
   }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { Country } from '../interfaces/pais.interface';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,8 +15,8 @@ export class PaisService {
 
   }
 
-  buscarPais(termino:string): Observable<any>{
+  buscarPais(termino:string): Observable<Country[]>{
     const url = `${this.apiUrl}/name/${termino}`
-    return this.http.get(url);
+    return this.http.get<Country[]>(url);
   }
 }
