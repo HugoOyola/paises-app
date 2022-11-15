5d125350f10b75c70de558f833c1f1005b38873b
Hugo Oyola <hugo.ham@outlook.com>
2022-11-15 00:56:42 -0500

    Mostrando paises por región

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -29,4 +29,9 @@ export class PaisService {
     const url = `${this.apiUrl}/alpha?codes=${id}`
     return this.http.get<Country[]>(url);
   }
+
+  buscarRegion(region:string): Observable<Country[]>{
+    const url = `${this.apiUrl}region/${region}`
+    return this.http.get<Country[]>(url);
+  }
 }
